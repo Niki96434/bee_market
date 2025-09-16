0d10d45b687e4acafb8c4d8a4567826f0309cdf9
Niki96434 <cozercaniegirl143@mail.ru>
2025-09-16 15:27:08 +0000

    черновик сайта

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask 
+from flask import url_for, render_template
+
+app = Flask(__name__)
+
+app.route('/')
+def homepage():
+    return render_template('news.html')
+
+app.route('/forum')
+def forum():
+    return render_template('forum.html')
+
+app.route('/video')
+def video():
+    return render_template('video.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5500)
